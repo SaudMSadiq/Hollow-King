bd7cd1796f460e1b1d3463f5538263bab81f910f
aryansharma2615-lab <aryan.sharma2615@gmail.com>
2026-03-17 17:59:36 -0400

    Update Player.cs
    added character animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,25 +9,118 @@ public class Player : MonoBehaviour
     public LayerMask groundLayer;
     private Rigidbody2D rb;
     private bool isGrounded;
+    private Animator animator;
+    private int attackCombo = 0;
+    private float attackTimer = 0f;
+    private float attackCooldown = 0.5f;
+    private bool isAttacking = false;
+    private bool isHurt = false;
+    private bool isDead = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        if (isDead) return;
 
+        float moveInput = Input.GetAxis("Horizontal");
+
+        // Movement (block during attack)
+        if (!isAttacking)
+        {
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        }
+
+        // Jump
         if (Input.GetKey(KeyCode.W) && isGrounded)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            animator.SetBool("Jump", true);
         }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+
+        // Running
+        if (Mathf.Abs(moveInput) > 0 && !isAttacking)
+            animator.SetInteger("AnimState", 1);
+        else if (!isAttacking)
+            animator.SetInteger("AnimState", 0);
+
+        // Flip direction
+        if (moveInput > 0)
+            transform.localScale = new Vector3(1, 1, 1);
+        else if (moveInput < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Attack combo
+        if (Input.GetMouseButtonDown(0) && !isHurt)
+        {
+            attackTimer = attackCooldown;
+            attackCombo++;
+            if (attackCombo > 3) attackCombo = 1;
+            isAttacking = true;
+
+            if (attackCombo == 1) animator.SetTrigger("Attack1");
+            else if (attackCombo == 2) animator.SetTrigger("Attack2");
+            else if (attackCombo == 3)
+            {
+                animator.SetTrigger("Attack3");
+                attackCombo = 0;
+            }
+        }
+
+        // Attack timer reset
+        if (attackTimer > 0)
+        {
+            attackTimer -= Time.deltaTime;
+            if (attackTimer <= 0)
+            {
+                isAttacking = false;
+                attackCombo = 0;
+            }
+        }
+
+        // Block
+        if (Input.GetMouseButton(1))
+            animator.SetBool("Block", true);
+        else
+            animator.SetBool("Block", false);
+
+        // Roll
+        if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)
+            animator.SetTrigger("Roll");
     }
+
     private void FixedUpdate()
     {
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        animator.SetBool("Grounded", isGrounded);
+    }
+
+    // Call this from enemy scripts to trigger hurt
+    public void TakeHit()
+    {
+        if (isDead) return;
+        isHurt = true;
+        animator.SetTrigger("Hurt");
+        Invoke("RecoverFromHurt", 0.5f);
+    }
+
+    private void RecoverFromHurt()
+    {
+        isHurt = false;
+    }
+
+    // Call this to kill the player
+    public void Die()
+    {
+        isDead = true;
+        animator.SetTrigger("Death");
     }
 }
