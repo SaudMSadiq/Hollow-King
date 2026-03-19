0b2c5948169f8f2d7a62ed4b15832cd94f70afc1
yzho465 <yzho465@uwo.ca>
2026-03-19 18:38:05 -0400

    Remaking NPCInteraction
    All the changes in NPCInteraction got overridden, so this is an attempt a remaking the branch

---
+++ Assets/Scripts/NPCDialogue.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewNPCDialogue", menuName = "NPC Dialogue")]
+public class NPCDialogue : ScriptableObject
+{
+   public string npcName;
+   public Sprite npcPortrait;
+   public string[] dialogueLines;
+   //public bool[] autoProgress;    // Optional: Array to determine if dialogue should auto-progress after each line
+   //public float autoProgressDelay = 1.5f; // Optional: Delay before auto-progressing to the next line
+   public float typingSpeed = 0.05f;
+   //public AudioClip voiceSound; // Optional: Sound effect for typing
+   //public float voicePitch = 1.0f; // Optional: Pitch for the voice sound
+   
+
+
+}
