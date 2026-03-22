1644b1b9d064bc7e39ad26ddf7a8fd90c2ddec84
Saud Muhammad Sadiq <smuham27@uwo.ca>
2026-03-22 17:08:06 -0400

    player speed and health values changed

---
+++ Assets/Scripts/CameraFollow 3.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform player;
+    public float smoothSpeed = 5f;
+
+    private float fixedY;
+    private float fixedZ;
+    
+    void Start()
+    {
+        fixedY = transform.position.y;
+        fixedZ = transform.position.z;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = new Vector3(player.position.x, fixedY, fixedZ);
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothSpeed);
+    }
+}
