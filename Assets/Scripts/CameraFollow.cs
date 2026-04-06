91acf9e7fd6f360dbcbbe69336fd2071ca0a04cc
Rob Worthy <168457467+reterk@users.noreply.github.com>
2026-04-06 14:09:18 -0400

    Revert "Merge remote-tracking branch 'origin/aryan-level2'"
    This reverts commit 7232cd78e6f13d4e1e87410410ef7e5113966f75, reversing
    changes made to b72f1336da1f930f5650d46643d786bcd318152e.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,13 +6,9 @@ public class CameraFollow : MonoBehaviour
     private Transform player;
     public float smoothSpeed = 4f;
 
-    public float minX;
-    public float maxX;
-    public float minY;
-    public float maxY;
-
+    private float fixedY;
     private float fixedZ;
-
+    
     void Start()
     {
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
@@ -25,10 +21,7 @@ public class CameraFollow : MonoBehaviour
 
     void LateUpdate()
     {
-        float targetX = Mathf.Clamp(player.position.x, minX, maxX);
-        float targetY = Mathf.Clamp(player.position.y, minY, maxY);
-
-        Vector3 targetPosition = new Vector3(targetX, targetY, fixedZ);
+        Vector3 targetPosition = new Vector3(player.position.x, fixedY, fixedZ);
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smoothSpeed);
     }
 }
